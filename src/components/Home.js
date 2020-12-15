3457da4bcbff28ee34697925d4c78361e830adf6
Andreas Steffensen <andreas.steffensen89@gmail.com>
2020-12-15 15:01:45 +0100

    added hero img

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import image from '../bgImage.jpg';
 
 export default function Home() {
-    return <h1>Home Page!</h1>
+    return (
+        <main>
+            <img src={image} alt="Lofoten beach sundown" className="absolute object-cover w-full h-full"/>
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className="text-6xl text-green-100 font-bold leading-none lg:leading-snug hero-title">I AM</h1>
+                <h2 className="absolute text-2xl text-green-100 font-bold leading-none lg:leading-snug ">Andreas Marius Steffensen</h2>
+            </section>
+        </main>
+    )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,77 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { SocialIcon } from "react-social-icons";
 
 export default function NavBar() {
-    return (
-        <header>
-            <div>
-                <nav>
-                    <NavLink to="/" exact>
-                        AMSteffensen
-                    </NavLink>
-                    <NavLink to="/post">
-                        Blog Posts
-                    </NavLink>
-                    <NavLink to="/projects">
-                        Projects
-                    </NavLink>
-                    <NavLink to="/about">
-                        About Me
-                    </NavLink>
-                </nav>
-            </div>
-        </header>
-    )
+  return (
+    <header>
+      <div className="container mx-auto flex justify-between">
+        <nav className="flex">
+          <NavLink
+            to="/"
+            exact
+            activeClassName="text-white"
+            className="inline-flex items-center py-6 px-3 mr-4 text-red-600 hover:text-green-800 text-4xl font-bold tracking-widest"
+          >
+            AMSteffensen
+          </NavLink>
+          <NavLink
+            to="/post"
+            activeClassName="bg-red-200 text-white-100"
+            className="inline-flex items-center py-3 px-3 rounded text-red-600 hover:text-green-800 text-2xl"
+          >
+            Blog Posts
+          </NavLink>
+          <NavLink
+            activeClassName="bg-red-200 text-white-100"
+            className="inline-flex items-center py-3 px-3 rounded text-red-600 hover:text-green-800 text-2xl"
+            to="/projects"
+          >
+            Projects
+          </NavLink>
+          <NavLink
+            activeClassName="bg-red-200 text-white-100"
+            className="inline-flex items-center py-3 px-3 rounded text-red-600 hover:text-green-800 text-2xl"
+            to="/about"
+          >
+            About Me
+          </NavLink>
+          <div className="inline-flex py-3 px-4 my-6">
+            <SocialIcon
+              url="http://linkedin.com/in/amsteffensen"
+              className="mr-4"
+              target="_blank"
+              bgColor="#dc2626"
+              fgColor="#fff"
+              style={{ height: 35, width: 35 }}
+            />
+            <SocialIcon
+              url="https://twitter.com/amsteffensen"
+              className="mr-4"
+              target="_blank"
+              bgColor="#dc2626"
+              fgColor="#fff"
+              style={{ height: 35, width: 35 }}
+            />
+            <SocialIcon
+              url="https://www.instagram.com/am.steffensen/"
+              className="mr-4"
+              target="_blank"
+              bgColor="#dc2626"
+              fgColor="#fff"
+              style={{ height: 35, width: 35 }}
+            />
+            <SocialIcon
+              url="https://github.com/AMSteffensen"
+              className="mr-4"
+              target="_blank"
+              bgColor="#dc2626"
+              fgColor="#fff"
+              style={{ height: 35, width: 35 }}
+            />
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
 }
